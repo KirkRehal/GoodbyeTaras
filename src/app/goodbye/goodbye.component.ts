4308f9d5441da9584b4b2f635e2d88567e1fd3ba
Kirk Rehal <rehal.kirk@gmail.com>
2022-01-05 20:56:44 -0600

    need quotes

--- src/app/goodbye/goodbye.component.ts
+++ src/app/goodbye/goodbye.component.ts
@@ -7,13 +7,18 @@ import { interval, Subscription, tap } from 'rxjs';
   styleUrls: ['./goodbye.component.css']
 })
 export class GoodbyeComponent implements OnInit {
-  public images: string[] = ['taras1.png'];
+  public images: string[] = ['taras1.png', 'taras2.png', 'taras3.png', 'taras4.png', 'taras5.png', 'taras6.png', 'taras7.png', 'taras12.png', 'taras9.png', 'taras10.png', 'taras11.png', 'taras8.png', 'taras13.png', 'taras14.png', ];
   public imageCounter: number = 0;
 
   public quotes: string[] = [`Daddy's home.`, `Kirk and Sri have always been the best.`, `Ukraine gives me ulcers...`];
   public quoteCounter: number = 0;
 
-  private counter = 0;
+  public funnyQuotes: string[] = [`Poor Anika`, `Department of 1`, `70 hour weeks`, `No vacations`, `Seriously.`];
+  public funnyQuoteCounter: number = 0;
+
+  public fin: boolean = false;
+
+  public counter = 0;
   private refreshTimer = 8000;
   private subscription: Subscription = new Subscription();
 
@@ -27,6 +32,9 @@ export class GoodbyeComponent implements OnInit {
         this.counter++;
         this.imageCounter = this.counter % this.images.length;
         this.quoteCounter = this.counter % this.quotes.length;
+        this.funnyQuoteCounter = this.counter % this.funnyQuotes.length;
+
+        this.fin = this.counter > this.imageCounter;
       })
     ).subscribe();
   }
